741f0835ac07749cb289fbee8ddfb8f2079b83ee
Uladzislau Korabau <vladkorobov.work@gmail.com>
2025-03-16 11:57:56 +0100

    added: service two and service three

---
+++ service-three/src/main/java/com/vladkorobovdev/service_three/ServiceThreeApplication.java
@@ -0,0 +1,13 @@
+package com.vladkorobovdev.service_three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceThreeApplication.class, args);
+	}
+
+}
---
+++ service-three/src/test/java/com/vladkorobovdev/service_three/ServiceThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladkorobovdev.service_three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
